2c6cf7b252d7d8e9f367dcacc7d0dd07e6191158
mhadihafeez <mhadihafeez@gmail.com>
2018-01-31 21:47:13 +0500

    post create edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,36 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.save
+    redirect_to posts_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    flash.notice = "Post '#{@post.title}' Updated!"
+    redirect_to posts_path(@post)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
